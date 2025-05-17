65460db5ea17c521e591a7de65fd4d4eabe0144b
mahmo <mahmoudalkurdgbg@gmail.com>
2025-05-17 22:15:37 +0200

    Initial CommitAdd project files.

---
+++ BankingConsoleApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using BankingConsoleApp.Database;
+
+class Program
+{
+    static void Main()
+    {
+        DbInitializer.CreateDatabase();
+        DbInitializer.CreateTables();
+
+
+        DbInitializer.InsertSampleData();
+
+        Console.WriteLine("🎉 Setup complete. Press any key to exit.");
+        Console.ReadKey();
+    }
+}
